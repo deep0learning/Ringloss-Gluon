f570053fc4e2585bdcd2f43ddeccb64c6e09e5fc
haoxintong <haoxintongpku@gmail.com>
2018-11-09 11:24:06 +0800

    update loss implement

--- src/loss.py
+++ src/loss.py
@@ -22,8 +22,7 @@
 """Custom loss"""
 
 import numpy as np
-from mxnet import nd, init
-from mxnet.gluon.loss import Loss
+from mxnet.gluon.loss import SoftmaxCrossEntropyLoss
 
 __all__ = ["RingLoss"]
 
@@ -61,23 +60,7 @@ def _apply_weighting(F, loss, weight=None, sample_weight=None):
     return loss
 
 
-def _reshape_like(F, x, y):
-    """Reshapes x to the same shape as y."""
-    return x.reshape(y.shape) if F is nd.ndarray else F.reshape_like(x, y)
-
-
-def _softmax_loss(F, pred, gt_label, weight, sample_weight, sparse_label, axis, batch_axis):
-    pred = F.log_softmax(pred, axis)
-    if sparse_label:
-        loss = -F.pick(pred, gt_label, axis=axis, keepdims=True)
-    else:
-        label = _reshape_like(F, gt_label, pred)
-        loss = -F.sum(pred * label, axis=axis, keepdims=True)
-    loss = _apply_weighting(F, loss, weight, sample_weight)
-    return F.mean(loss, axis=batch_axis, exclude=True)
-
-
-class RingLoss(Loss):
+class RingLoss(SoftmaxCrossEntropyLoss):
     """Computes the Ring Loss from
     `"Ring loss: Convex Feature Normalization for Face Recognition"
     <https://arxiv.org/abs/1803.00130>`_paper.
@@ -93,73 +76,29 @@ class RingLoss(Loss):
     ----------
     lamda: float
         The loss weight enforcing a trade-off between the softmax loss and ring loss.
-    axis : int, default -1
-        The axis to sum over when computing softmax and entropy.
-    sparse_label : bool, default True
-        Whether label is an integer array instead of probability distribution.
-    from_logits : bool, default False
-        Whether input is a log probability (usually from log_softmax) instead
-        of unnormalized numbers.
-    weight : float or None
-        Global scalar weight for loss.
-    batch_axis : int, default 0
-        The axis that represents mini-batch.
-
-
-    Inputs:
-        - **pred**: the prediction tensor, where the `batch_axis` dimension
-          ranges over batch size and `axis` dimension ranges over the number
-          of classes.
-        - **label**: the truth tensor. When `sparse_label` is True, `label`'s
-          shape should be `pred`'s shape with the `axis` dimension removed.
-          i.e. for `pred` with shape (1,2,3,4) and `axis = 2`, `label`'s shape
-          should be (1,2,4) and values should be integers between 0 and 2. If
-          `sparse_label` is False, `label`'s shape must be the same as `pred`
-          and values should be floats in the range `[0, 1]`.
-        - **embedding**: the output of embedding layer before classification.
-          It should be (batch size, feature size) shape.
-        - **sample_weight**: element-wise weighting tensor. Must be broadcastable
-          to the same shape as label. For example, if label has shape (64, 10)
-          and you want to weigh each sample in the batch separately,
-          sample_weight should have shape (64, 1).
 
     Outputs:
-        - **loss**: loss tensor with shape (batch_size,). Dimenions other than
+        - **loss**: loss tensor with shape (batch_size,). Dimensions other than
           batch_axis are averaged out.
 
     """
 
-    def __init__(self, lamda, weight=None, batch_axis=0,
-                 axis=-1, sparse_label=True, from_logits=False,
-                 weight_initializer=init.Constant(1.0), dtype='float32', **kwargs):
-        super().__init__(weight=weight, batch_axis=batch_axis, **kwargs)
-        # Softmax
-        self._axis = axis
-        self._sparse_label = sparse_label
-        self._from_logits = from_logits
+    def __init__(self, lamda, weight_initializer=None, dtype='float32',
+                 axis=-1, sparse_label=True, weight=None, batch_axis=0, **kwargs):
+        super().__init__(axis=axis, sparse_label=sparse_label, weight=weight, batch_axis=batch_axis, **kwargs)
 
-        # RingLoss
         self._lamda = lamda
-        self.R = self.params.get('R', shape=(1,),
-                                 init=weight_initializer, dtype=dtype,
-                                 allow_deferred_init=True)
+        self.R = self.params.get('R', shape=(1,), init=weight_initializer,
+                                 dtype=dtype, allow_deferred_init=True)
 
     def hybrid_forward(self, F, pred, label, embedding, R, sample_weight=None):
-
         # RingLoss
         emb_norm = F.norm(embedding, axis=1)
         loss_r = F.square(F.broadcast_sub(emb_norm, R))
-        loss_r = F.mean(loss_r, keepdims=True) * 0.5
+        loss_r = loss_r * 0.5
+        loss_r = _apply_weighting(F, loss_r, self._weight, sample_weight)
 
         # Softmax
-        if not self._from_logits:
-            pred = F.log_softmax(pred, self._axis)
-        if self._sparse_label:
-            loss_sm = -F.pick(pred, label, axis=self._axis, keepdims=True)
-        else:
-            label = _reshape_like(F, label, pred)
-            loss_sm = -F.sum(pred * label, axis=self._axis, keepdims=True)
-        loss_sm = F.mean(loss_sm, axis=self._batch_axis, exclude=True)
+        loss_sm = super().hybrid_forward(F, pred, label, sample_weight)
 
-        loss = F.broadcast_add(loss_sm, self._lamda * loss_r)
-        return _apply_weighting(F, loss, self._weight, sample_weight)
+        return loss_sm + self._lamda * loss_r
